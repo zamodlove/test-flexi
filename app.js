222f0e6f25e7ed32c2a27f10f475d16038923f72
modGN5555 <mod@GGG.com>
2024-04-19 16:00:10 +0700

    js-basic v1

---
+++ app.js
@@ -0,0 +1,15 @@
+// let number = Array(100,200,300,400,500,600)
+// console.log(number[0]);
+
+
+let colors = ['red','pink','green','blue']
+colors[1] = 'เหลือง'
+console.log(colors)
+console.log(colors[1]) 
+
+
+
+let days = ['','','','','']
+
+
+
